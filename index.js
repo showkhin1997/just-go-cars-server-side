ca6b1d287e232743a3296ae2f75cbd96ce00b491
showkhin1997 <showkhin15@gmail.com>
2021-11-10 16:50:40 +0600

    api add

--- index.js
+++ index.js
@@ -11,6 +11,35 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6re1r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("justGoCars");
+        const productsCollection = database.collection("products");
+
+        // POST API
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
+            res.json(result);
+        });
+
+        // GET API
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+    }
+    finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Car Sales Server Runnig');
@@ -18,4 +47,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log('listening Port', port)
-})
+});
