4b4c492e49ed7946652c6923f1532272285f4915
showkhin1997 <showkhin15@gmail.com>
2021-11-12 00:09:50 +0600

    admin add

--- index.js
+++ index.js
@@ -108,6 +108,25 @@ async function run() {
             res.json(result);
         });
 
+        app.post('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        });
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        });
+
         // DELETE API
         app.delete('/order/:id', async (req, res) => {
             const id = req.params.id;
